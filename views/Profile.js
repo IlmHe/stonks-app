0fdd7caf9295be2022621a6a8c6eacca759f86f4
ohuji <juhsalomaki@gmail.com>
2022-10-03 14:37:07 +0300

    asd

--- views/Profile.js
+++ views/Profile.js
@@ -1,19 +1,18 @@
 import {Platform, StyleSheet, View, SafeAreaView, Text} from 'react-native';
 import PropTypes from 'prop-types';
-import React, { Component } from "react";
-
+import React, {Component} from 'react';
 
 const Profile = (props) => {
   const {navigation} = props;
   return (
     <View style={styles.container}>
-      <View style={styles.undefined}/>
-      <View style={styles.undefined}/>
+      <View style={styles.undefined} />
+      <View style={styles.undefined} />
       <Text style={styles.placeholderName}>PlaceholderName</Text>
       <Text style={styles.followers}>Followers</Text>
       <Text style={styles.loremIpsum}>23</Text>
-      <View style={styles.rect}/>
-      <View style={styles.rect2}/>
+      <View style={styles.rect} />
+      <View style={styles.rect2} />
       <Text style={styles.follow2}>FOLLOW!</Text>
     </View>
   );
@@ -26,53 +25,53 @@ const styles = StyleSheet.create({
     paddingTop: Platform.OS === 'android' ? 30 : 0,
   },
   container: {
-    flex: 1
+    flex: 1,
   },
   undefined: {},
   placeholderName: {
     top: 310,
     left: 127,
-    position: "absolute",
-    fontFamily: "roboto-regular",
-    color: "#121212"
+    position: 'absolute',
+    fontFamily: 'roboto-regular',
+    color: '#121212',
   },
   followers: {
     top: 340,
     left: 127,
-    position: "absolute",
-    fontFamily: "roboto-regular",
-    color: "#121212"
+    position: 'absolute',
+    fontFamily: 'roboto-regular',
+    color: '#121212',
   },
   loremIpsum: {
     top: 340,
     left: 222,
-    position: "absolute",
-    fontFamily: "roboto-regular",
-    color: "#121212"
+    position: 'absolute',
+    fontFamily: 'roboto-regular',
+    color: '#121212',
   },
   rect: {
     top: 460,
     left: 56,
     width: 263,
     height: 190,
-    position: "absolute",
-    backgroundColor: "#E6E6E6"
+    position: 'absolute',
+    backgroundColor: '#E6E6E6',
   },
   rect2: {
     top: 102,
     left: 101,
     width: 173,
     height: 161,
-    position: "absolute",
-    backgroundColor: "#E6E6E6"
+    position: 'absolute',
+    backgroundColor: '#E6E6E6',
   },
   follow2: {
     top: 398,
     left: 153,
-    position: "absolute",
-    fontFamily: "roboto-regular",
-    color: "#121212"
-  }
+    position: 'absolute',
+    fontFamily: 'roboto-regular',
+    color: '#121212',
+  },
 });
 
 Profile.propTypes = {
